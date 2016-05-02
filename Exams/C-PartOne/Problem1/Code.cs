1a328bf664c79f0276e033cc7ee25a3c3ad44913
YPenev <dany.penev@gmail.com>
2016-05-02 16:39:44 +0300

    Create Code.cs

---
+++ Exams/C-PartOne/Problem1/Code.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        double B = double.Parse(Console.ReadLine());
+        double F = double.Parse(Console.ReadLine());
+        double avgF = F / B;
+        
+
+        if (B % 2 == 0)
+        {
+            avgF *= 123123123123;
+        }
+        else
+        {
+            avgF /= 317;
+        }
+
+        Console.WriteLine("{0:0.0000}",avgF);
+    }
+}
