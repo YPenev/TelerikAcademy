0413d3d2c3eb941f152131a662bb1784e6d52409
YPenev <dany.penev@gmail.com>
2016-05-02 16:53:36 +0300

    Create MyCode.cs

---
+++ Exams/C-PartOne/Problem3/MyCode.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        int d = int.Parse(Console.ReadLine());
+        long digits = 0;
+        int page =  1;
+        int temp;
+        int diggitOfPage = 0;
+
+        while (digits != d)
+        {
+            temp = page;
+
+            while (temp!=0)
+            {
+                diggitOfPage++;
+                temp /= 10;
+            }
+            digits = digits + diggitOfPage;
+            diggitOfPage = 0;
+            page++;
+        }
+
+
+        Console.WriteLine(page-1);
+    }
+}
